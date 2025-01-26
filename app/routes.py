ff761b10e832e489bf4abcb37ec369066a2a8714
Marcosespa <marcosespa24@gmail.com>
2025-01-25 21:13:25 -0500

    Cambio en el dateTime

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app import db
 from app.models import Usuario, Tarea, Categoria
+from datetime import datetime
 
 main_routes = Blueprint('main', __name__)
 
@@ -18,19 +19,66 @@ def obtener_tareas(usuario_id):
     } for tarea in tareas])
 
 # Endpoint para crear una tarea
+
 @main_routes.route('/tareas', methods=['POST'])
 @jwt_required()
 def crear_tarea():
     data = request.get_json()
+
+    # Verificar si se proporcionó un id_categoria
+    if 'id_categoria' in data:
+        # Verificar si la categoría existe
+        categoria = Categoria.query.get(data['id_categoria'])
+        if not categoria:
+            return jsonify({"mensaje": "La categoría no existe"}), 400  # 400: Bad Request
+
+    # Convertir la cadena de fecha a un objeto date
+    try:
+        fecha_creacion = datetime.strptime(data['fecha_creacion'], '%Y-%m-%d').date()
+    except ValueError:
+        return jsonify({"mensaje": "Formato de fecha inválido. Use 'YYYY-MM-DD'"}), 400
+
+    # Crear la tarea
     nueva_tarea = Tarea(
         texto_tarea=data['texto_tarea'],
-        fecha_creacion=data['fecha_creacion'],
+        fecha_creacion=fecha_creacion,  # Usar el objeto date
         id_usuario=data['id_usuario'],
-        id_categoria=data.get('id_categoria')
+        id_categoria=data.get('id_categoria')  # Opcional
     )
+
+    # Guardar la tarea en la base de datos
     db.session.add(nueva_tarea)
     db.session.commit()
+
     return jsonify({"mensaje": "Tarea creada"}), 201
+
+# @main_routes.route('/tareas', methods=['POST'])
+# @jwt_required()
+# def crear_tarea():
+#     data = request.get_json()
+
+#     # Verificar si se proporcionó un id_categoria
+#     if 'id_categoria' in data:
+#         # Verificar si la categoría existe
+#         categoria = Categoria.query.get(data['id_categoria'])
+#         if not categoria:
+#             return jsonify({"mensaje": "La categoría no existe"}), 400  # 400: Bad Request
+
+#     # Crear la tarea
+#     nueva_tarea = Tarea(
+#         texto_tarea=data['texto_tarea'],
+#         fecha_creacion=data['fecha_creacion'],
+#         id_usuario=data['id_usuario'],
+#         id_categoria=data.get('id_categoria')  # Opcional
+#     )
+
+#     # Guardar la tarea en la base de datos
+#     db.session.add(nueva_tarea)
+#     db.session.commit()
+
+#     return jsonify({"mensaje": "Tarea creada"}), 201
+
+
 
 # Endpoint para eliminar una tarea
 @main_routes.route('/tareas/<int:tarea_id>', methods=['DELETE'])
